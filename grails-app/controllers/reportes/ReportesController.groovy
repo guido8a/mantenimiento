a0b3477f908c951480f64ebe1a95c23519f8dfb7
Fabricio <fegrijalva2501@hotmail.com>
2026-03-23 09:04:03 -0500

    reporte actividades

--- grails-app/controllers/reportes/ReportesController.groovy
+++ grails-app/controllers/reportes/ReportesController.groovy
@@ -1861,7 +1861,9 @@ class ReportesController {
         headersTitulo.setAlignment(Element.ALIGN_CENTER);
         headersTitulo.add(new Paragraph("REPORTE DE ACTIVIDADES", titleFont));
         addEmptyLine(headersTitulo, 1);
-        headersTitulo.add(new Paragraph("Reporte generado: " + new Date().format("dd-MM-yyyy"), titleFont3));
+        headersTitulo.add(new Paragraph("Actividades asociadas al término:  ${params.clave}", titleFont3));
+        addEmptyLine(headersTitulo, 1);
+        headersTitulo.add(new Paragraph("Reporte generado el " + new Date().format("dd-MM-yyyy"), titleFont3));
         addEmptyLine(headersTitulo, 1);
 //        headersTitulo.add(new Paragraph(oficio?.contrato?.objeto, titleFont3Normal));
 //        addEmptyLine(headersTitulo, 1);
